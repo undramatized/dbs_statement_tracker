7307bdf85ff8b1d203830cbf7520392009560c1a
Rama Krishna <siddarth21@hotmail.com>
2020-03-25 19:18:04 +0800

    Added script for extracting investment data into json

---
+++ src/extract_data.py
@@ -0,0 +1,116 @@
+import re
+import json
+'''
+ExtractData
+
+This class initiates with:
+- filename of PDF file (excl .pdf extension)
+
+Class executes the following:
+- Retrieves a PDF file from the /statement_files folder
+- Converts PDF file into plaintext
+- Stores plaintext file in /processed_files/raw folder
+
+'''
+
+class ExtractData:
+    TEXT_FILE_PATH = "../processed_files/raw/{filename}.txt"
+    PYPDF_TEXT_FILE_PATH = "../processed_files/raw/{filename}_pypdf.txt"
+    JSON_PATH = "../processed_files/compiled/investment_data.json"
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def execute(self):
+        print("running python parser")
+        file_path = self.TEXT_FILE_PATH.format(filename=self.filename)
+
+        file = open(file_path, 'r')
+        content = file.readlines()
+        file.close()
+
+        processed_content = self.process_text(content)
+
+        investment_data = self.get_investment_data(processed_content)
+        investment_amount = self.get_investment_amount()
+
+        investment_data["inv_amount"] = investment_amount
+        print(investment_data)
+
+        self.append_to_json(investment_data)
+
+    def process_text(self, content):
+        content = self.remove_newlines(content)
+        content = self.remove_page_info(content)
+        return content
+
+    def remove_newlines(self, content):
+        return [line for line in content if line != '\n']
+
+    def remove_page_info(self, content):
+        filtered = []
+        pending_delete = 0
+        for index, line in enumerate(content):
+            if pending_delete == 1:
+                pending_delete = 0
+            elif line[:4] == 'Page':
+                pending_delete = 1
+            else:
+                filtered.append(line)
+        return filtered
+
+    def get_investment_data(self, content):
+        extracted_data = []
+        for index, line in enumerate(content):
+            if line[:5]=='NIKKO':
+                for i in range(10):
+                    val = content[index+i].strip()
+                    if val.replace('.', '', 1).replace(',', '', 1).isdigit():
+                        extracted_data.append(val)
+                break
+        print(extracted_data)
+        investment_data = {
+            'datetime': self.filename,
+            'no_of_units': extracted_data[0].strip(),
+            'current_price': extracted_data[1].strip(),
+            'total_value': extracted_data[2].strip(),
+        }
+        return investment_data
+
+    def get_investment_amount(self):
+        # Looks through transaction records to identify the "Unit Trust Application" transaction
+        # Ensures that subsequent value is in 100s range (eg. 150.00 or 300.00)
+        # Defaults to 100.00
+
+        file_path = self.PYPDF_TEXT_FILE_PATH.format(filename=self.filename)
+        file = open(file_path, 'r')
+        content = file.read()
+        file.close()
+
+        pattern = '(Unit Trust Application)'
+
+        split_string_on_regex = re.split(pattern, content)
+        for index, line in enumerate(split_string_on_regex):
+            if line == "Unit Trust Application":
+                next_line = split_string_on_regex[index+1]
+                print(next_line)
+                if next_line[3] == '.':
+                    amount = next_line[:6]
+                    return amount
+
+        return '100.00'
+
+    def append_to_json(self, data):
+        json_data = ''
+        with open(self.JSON_PATH, 'r+', encoding='utf-8') as f:
+            json_data = json.load(f)
+            json_data.append(data)
+
+        self.write_json(json_data)
+
+    def write_json(self, data):
+        with open(self.JSON_PATH, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+if __name__ == '__main__':
+    ExtractData("2020_01").execute()
--- src/pdf_to_text.py
+++ src/pdf_to_text.py
@@ -1,26 +1,42 @@
 from PyPDF2 import PdfFileReader
 import textract
 
+'''
+PDFtoText
+
+This class initiates with: 
+- filename of PDF file (excl .pdf extension)
+
+Class executes the following:
+- Retrieves a PDF file from the /statement_files folder
+- Converts PDF file into plaintext
+- Stores plaintext file in /processed_files/raw folder
+
+'''
+
 class PDFtoText:
     PDF_PATH = "../statement_files/{filename}.pdf"
     TEXT_FILE_PATH = "../processed_files/raw/{filename}.txt"
 
-    def __init__(self):
-        self.filename = "2020_01"
+    def __init__(self, filename):
+        self.filename = filename
 
     def execute(self):
-        print("running python parser")
+        print("running pdf to text")
         file_path = self.PDF_PATH.format(filename=self.filename)
 
-        # pdf = open(file_path, 'rb')
-        # page_count = self.get_page_count(pdf)
-        # extracted_text = self.extract_text_pypdf(pdf, 0)
+        pdf = open(file_path, 'rb')
+        page_count = self.get_page_count(pdf)
+        for i in range(page_count):
+            extracted_text = self.extract_text_pypdf(pdf, i)
+            self.append_text_to_file(extracted_text, "_pypdf")
+
 
         extracted_text = self.extract_text_textract(file_path)
         decoded_text = extracted_text.decode('utf-8')
         self.append_text_to_file(decoded_text)
 
-        # pdf.close()
+        pdf.close()
 
     def get_page_count(self, pdf):
         pdf_reader = PdfFileReader(pdf)
@@ -43,10 +59,10 @@ class PDFtoText:
 
         return text
 
-    def append_text_to_file(self, text):
-        text_file = open(self.TEXT_FILE_PATH.format(filename=self.filename), "a")
+    def append_text_to_file(self, text, prefix=''):
+        text_file = open(self.TEXT_FILE_PATH.format(filename=(self.filename+prefix)), "a")
         text_file.write(text)
         text_file.close()
 
 if __name__ == '__main__':
-    PDFtoText().execute()
+    PDFtoText("2015_08").execute()
